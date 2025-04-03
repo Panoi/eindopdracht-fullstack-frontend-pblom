8edad175f557149e6edf220f37f6636d3e65f004
panoi <pascal_blom9@hotmail.com>
2025-04-03 11:53:50 +0200

    added link list to navbar.jsx and styling to navbar.css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,22 @@
 import {Link} from "react-router-dom";
+import "./NavBar.css"
 
 
 function NavBar(){
 
     return (
-        <nav>
+        <nav className="navbar">
             <Link to='/'>
                 GemeenteApp
             </Link>
-
-
+            <ul className="navbar-list">
+                <li><Link to='/login'>Login</Link></li>
+                <li><Link to='/register'>Registreren</Link></li>
+                <li><Link to='/profile'>Profiel</Link></li>
+                <li><Link to='/proposal'>Verzoek</Link></li>
+                <li><Link to='/proposals'>Verzoekoverzicht</Link></li>
+            </ul>
         </nav>
-
 
 
     )
