763d0b840f9b03a133058bb3db7b7f67ee3a3b4a
panoi <pascal_blom9@hotmail.com>
2025-04-04 15:03:21 +0200

    added logic to UserForm component

--- src/components/userform/UserForm.jsx
+++ src/components/userform/UserForm.jsx
@@ -1,11 +1,20 @@
 import "./UserForm.css"
 
-function UserForm() {
+function UserForm({inputType, inputName, inputLabel, inputId, validationRules, register, errors}) {
 
     return (
         <>
+            <label htmlFor={inputId} className="form-label">
+                {inputLabel}
+                <input
+                    type={inputType}
+                    id={inputId}
+                    {...register(inputName, validationRules)}
+                />
+            </label>
+            {errors[inputName] && <p>{errors[inputName].message}</p>}
         </>
-    )
+    );
 }
 
 export default UserForm
